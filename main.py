629f7befe45b6289a458ac9df0773c9b7c9dcc76
jesho-cpu <157772768+jesho-cpu@users.noreply.github.com>
2024-02-24 06:29:11 +0000

    Updated Code for Exercise Lab 4

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    
+     # Extract "Python" from original string with index slicing
+    sub1 = original_str[:6]
+
+    # Extract "Programming" from original string with index slicing
+    sub2 = original_str[7:]
+
+    # Using string concatenation('+'), merge two substrings sub1 and sub2
+    merged_str = sub2 + " " + sub1
 
     print(sub2)
     print(sub1)
